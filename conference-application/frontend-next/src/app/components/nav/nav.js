2b45270964207e359746c370b75baea2693c226a
Ezequiel <ezequiel.salatino@gmail.com>
2023-06-19 12:53:12 +0200

    Navigation styles

--- conference-application/frontend-next/src/app/components/nav/nav.js
+++ conference-application/frontend-next/src/app/components/nav/nav.js
@@ -1,30 +1,41 @@
 
+'use client'
 import styles from './nav.module.css'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation'
 
 
 
 export default function Nav() {
-    
-    
+    const pathname = usePathname()
+    console.log("path", pathname);
     return (
-        <>
         <nav className={styles.nav}>
-            <ul className={styles.menu}>
-                <li className={styles.menuItem}><Link href="/" scroll={false}>Home</Link></li>
-                <li className={styles.menuItem}><Link href="/about/" scroll={false}>About</Link></li>
-                <li className={styles.menuItem}><Link href="/agenda/" scroll={false}>Agenda</Link></li>
-                <li className={styles.menuItem}><Link href="/proposals/" scroll={false}>Proposals</Link></li>
-                <li className={styles.menuItem}><Link href="/backend/" scroll={false}>Backend</Link></li>
-            </ul>
-        </nav>
+        <div className="grid">
+            <div className="col third">
+                <ul className={styles.logos}>
+                     <li className={styles.logosItem} ><Link href="/"  className={pathname === "/" ? `${styles.active} ` : ' '} scroll={false}>Logos</Link></li>
+                </ul>
+            </div>
+            <div className="col half positionHalf">
+                
+                    <ul className={styles.menu}>
+                        <li className={styles.menuItem}><Link href="/about/" className={pathname === "/about" ? `${styles.active} ` : ' '} scroll={false}>About</Link></li>
+                        <li className={styles.menuItem}><Link href="/agenda/" className={pathname === "/agenda" ? `${styles.active} ` : ' '} scroll={false}>Agenda</Link></li>
+                        <li className={styles.menuItem}><Link href="/proposals/" className={pathname === "/proposals" ? `${styles.active} ` : ' '} scroll={false}>Proposals</Link></li>
+                        <li className={styles.menuItem}><Link href="/backend/" className={pathname === "/backend" ? `${styles.active} ` : ' '} scroll={false}>Backend</Link></li>
+                    </ul>
+                
+            </div>
+            
+        </div>
             
      
                     
             
-        </>
+        </nav>    
         
     
     );
 }
+
