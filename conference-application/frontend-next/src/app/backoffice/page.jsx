a2af75628e19b99c04a3d5f3e40064f6051c08af
Ezequiel <ezequiel.salatino@gmail.com>
2023-06-22 14:55:22 +0200

    Starting with backoffice styles

--- conference-application/frontend-next/src/app/backoffice/page.jsx
+++ conference-application/frontend-next/src/app/backoffice/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import styles from '@/app/styles/backend.module.css'
+import styles from '@/app/styles/backoffice.module.css'
 import { useState, useEffect } from 'react'
 
 import ProposalList from '../components/proposals/proposallist'
@@ -8,7 +8,7 @@ import NotificationList from '../components/notifications/notificationlist'
 import EventsList from '../components/events/eventlist'
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+
 
 
 export default function Backoffice() {
@@ -18,33 +18,39 @@ export default function Backoffice() {
 
   return (
     <main className={styles.main}>
+      <div className={`${styles.hero} ` }>
+        <div className={ `grid content noMargin`}>
+          <div className="col full">
+            <h3>Backoffice</h3>
+          </div>
+        </div>
+      </div>
+      <div className={`${styles.BackofficeContent} ` }>
+      <div className={ `grid content noMargin`}>
+        <div className="col full">
+          <div className={`${styles.tabs} ` }>
+          <Tabs>
+            <TabList>
+              <Tab>Review Proposals</Tab>
+              <Tab>Notifications</Tab>
+              <Tab>Events</Tab>
+            </TabList>
 
-      <Tabs>
-        <TabList>
-          <Tab>Review Proposals</Tab>
-          <Tab>Notifications</Tab>
-          <Tab>Events</Tab>
-        </TabList>
-
-        <TabPanel>
-          <ProposalList></ProposalList>
-        </TabPanel>
-        <TabPanel>
-          <NotificationList />
-        </TabPanel>
-        <TabPanel>
-          <EventsList />
-        </TabPanel>
-      </Tabs>
+            <TabPanel>
+              <ProposalList></ProposalList>
+            </TabPanel>
+            <TabPanel>
+              <NotificationList />
+            </TabPanel>
+            <TabPanel>
+              <EventsList />
+            </TabPanel>
+          </Tabs>
+          </div>
+        </div>
+      </div>
+      </div>
      
-      
-            
-         
-            
-          
-
-      
-
 
 
     </main>
